e23b34d4c9ab602bf30f3b689be4e6d4f1a72d91
Kozmik <koz.moore@gmail.com>
2025-06-25 20:25:58 -0700

    First run using local environment. Added title to notebook.

--- code/utilities/config.py
+++ code/utilities/config.py
@@ -1,1 +1,15 @@
+from pathlib import Path
 from os.path import join
+
+# Directory paths
+root_dir = Path('..')
+data_dir = Path(join(root_dir, 'data'))
+assets_dir = Path(join(root_dir, 'assets'))
+code_dir = Path(join(root_dir, 'code'))
+products_dir = Path(join(root_dir, 'products'))
+
+# File paths
+rawdata_file = join(data_dir, 'bank_marketing.csv')
+campaign_file = join(data_dir, 'campaign.csv')
+client_file = join(data_dir, 'client.csv')
+economic_file = join(data_dir, 'economic.csv')
